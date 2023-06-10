51f8f603b4549f42c3efaea9e6883a9cb656f089
Ricardo Gurgel <ricardo.gurgel@eletraenergy.com>
2023-06-10 09:52:47 -0300

    Adicionando estruturas base para biblioteca Password.

---
+++ Password/Password.cpp
@@ -0,0 +1,17 @@
+\*
+
+Biblioteca de administração de senha para teclado matricial 4x4.
+
+Autores: El Profesor, Daltonico, Milico & Comunista.
+Junho de 2023
+
+*/
+
+#include "Arduino.h"
+#include "Password.h"
+
+Password::Password()
+{
+
+}
+
---
+++ Password/Password.h
@@ -0,0 +1,26 @@
+\*
+
+Biblioteca de administração de senha para teclado matricial 4x4.
+
+Autores: El Profesor, Daltonico, Milico & Comunista.
+Junho de 2023
+
+*/
+
+
+#ifndef Password
+#define Password
+
+#include "Arduino.h"
+
+class Password
+{
+  public:
+
+
+  private:
+
+
+};
+
+#endif
